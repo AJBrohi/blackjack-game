4aefb1655c68958142f46f073b3d44c4e338d21f
AJBrohi <brohi96@gmail.com>
2023-04-25 03:15:16 +0600

    code done

---
+++ main.py
@@ -0,0 +1,91 @@
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+################################################################
+
+import random, os, art
+
+card = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "A": [1, 11], "K": 10, "Q": 10, "J": 10}
+
+def card_distribution(score):
+    random_card = random.choice(list(card.keys()))
+    if score < 11 and random_card == "A":
+        score += card[random_card][1]
+    elif score >= 11 and random_card == "A":
+        score += card[random_card][0]
+    else:
+        score += card[random_card]
+    
+    return random_card, score
+
+def print_cards(final, user_score, pc_score, user_inhand, pc_inhand):
+    if final:
+        print(f"Your Final Cards: {user_inhand}, Current Score: {user_score}")
+        print(f"PC's Final Cards: {pc_inhand}, PC's Score: {pc_score}")
+    else:
+        print(f"Your Cards: {user_inhand}, Current Score: {user_score}")
+        print(f"PC's Card: {pc_inhand[0]}")
+
+def blackjack():
+    print(art.logo)
+    user_inhand = []
+    user_score = 0
+    pc_inhand = []
+    pc_score = 0
+    
+    clear = lambda: os.system('cls')
+    
+    should_play = True
+    while should_play:
+        for _ in range(2):
+            user_card, user_score = card_distribution(score=user_score)
+            user_inhand.append(user_card)
+            
+            pc_card, pc_score = card_distribution(score=pc_score)
+            pc_inhand.append(pc_card)
+        
+        print_cards(False, user_score=user_score, pc_score=pc_score, user_inhand=user_inhand, pc_inhand=pc_inhand)
+        
+        while user_score < 21:
+            choice = input("Type 'Y' to HIT another card or 'N' to PASS - ").lower()
+            # choice = 'y'
+            if choice == 'y':
+                user_card, user_score = card_distribution(score=user_score)
+                user_inhand.append(user_card)
+            else:
+                while pc_score <= 16:
+                    pc_card, pc_score = card_distribution(score=pc_score)
+                    pc_inhand.append(pc_card)
+                break
+            print_cards(False, user_score=user_score, pc_score=pc_score, user_inhand=user_inhand, pc_inhand=pc_inhand)
+
+        print_cards(True, user_score=user_score, pc_score=pc_score, user_inhand=user_inhand, pc_inhand=pc_inhand)
+
+        if pc_score > 21:
+            print("PC went over. You Win")
+        elif user_score > 21:
+            print("You went over. PC Win")
+        elif user_score == 21 and pc_score == 21 or user_score == pc_score:
+            print("Draw!")
+        elif pc_score > user_score:
+            print("PC Win")
+        elif user_score > pc_score:
+            print("You Win")
+            
+        round_choice = input("Type 'Y' to play another round or type 'N' to exit - ").lower()        
+        if round_choice == 'y':
+            clear()
+            blackjack()
+        else:
+            should_play = False
+            print("Thank you for playing the game. Hope you liked it. Goodbye!")
+blackjack()
